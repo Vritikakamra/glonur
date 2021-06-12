51e501a3e98ded111e76e73427a05b18c924ed7e
aravindhippili <aravindhippili@gmail.com>
2021-06-12 12:50:28 +0530

    added api

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-//import 'package:glonur/pages/landing_page.dart';
-import 'package:glonur/pages/appbar.dart';
+import 'package:glonur/pages/home.dart';
+import 'package:glonur/pages/settings.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -9,9 +10,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body:Appbarpage(), 
-        //HomePage(),
+        appBar: AppBar(
+          title: Text("Glonur"),
+          centerTitle: true,
+          actions: [
+            IconButton(icon: Icon(Icons.search), onPressed: () {}),
+            IconButton(icon: Icon(Icons.notifications_none), onPressed: () {}),
+          ],
+        ),
+        drawer: Settings(),
+        body: HomePage(),
       ),
     );
   }
